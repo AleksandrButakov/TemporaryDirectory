9d5484ef906a15d9865fa66d022c22b4ebcd84a5
GTemp <anbngm@gmail.com>
2021-12-06 22:31:04 +0500

    First work step

--- src/test/java/TestTempRSHB.java
+++ src/test/java/TestTempRSHB.java
@@ -1,6 +1,10 @@
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.json.JsonOutput;
 import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -9,13 +13,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class TestTempRSHB {
-
     WebDriver driver;
     WebElement element;
     WebDriverWait driverWait;
+
     @Test
     public void webTest() {
 
@@ -28,20 +33,25 @@ public class TestTempRSHB {
         driver.get("https://www.rshb.ru/");
 
         /*
-        // согласимся с cookies
-        //*[@id="alert"]/div/div/div[2]/button
-        //*[@id="alert"]/div/div/div[2]/button
+        работа со всплывающими окнами
+        // Alert alert = driver.switchTo().alert();
+        Alert alert = (new WebDriverWait(driver, 10)).until(ExpectedConditions.alertIsPresent());
+        alert.dismiss();
+        alert accept();
          */
-            // driver.findElement((By.xpath("//*[@id=\"alert\"]/div/div/div[2]/button")));
-        //driverWait.until(ExpectedConditions.visibilityOf(element));
-        //element.click();
+
+        // ожидаем окно принятие cookies
+        System.out.println("Окно cookies");
+        createPause();
+        element = driver.findElement(By.xpath("//*[@id=\"alert\"]/div/div/div[2]/button"));
+        driverWait.until(ExpectedConditions.visibilityOf(element));
+        element.click();
 
         System.out.println("Step_01");
         System.out.println("Частным лицам");
         createPause();
         // Частным лицам
         // $$("div.b-main-menu-items [href='/natural/']")
-        //driver.findElement(By.cssSelector("div.b-main-menu-items [href='/natural/']"));
         element = driver.findElement(By.cssSelector("div.b-main-menu-items [href='/natural/']"));
         driverWait.until(ExpectedConditions.visibilityOf(element));
         element.click();
@@ -77,6 +87,12 @@ public class TestTempRSHB {
             System.out.println("!!! Элемент не кликабельный");
         }
 
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        driver.manage().deleteAllCookies();
+        // указываем какой скрипт необходимо выполнить. Необходимо почистить все сессионные данные, которые
+        // которые находятся в определенном окне
+        javascriptExecutor.executeScript("window.sessionStorage.clear()");
+
         createPause();
         driver.close();
     }
@@ -88,4 +104,6 @@ public class TestTempRSHB {
             e.printStackTrace();
         }
     }
+
+
 }
